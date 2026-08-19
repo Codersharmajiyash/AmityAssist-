3cb15ceacdfaea341669d2ee6187193d412835f5
Yash Sharma <yash.sharma64@s.amity.edu>
2026-08-19 23:56:10 +0530

    Revert "PHASE 15,16,17 completed"

--- frontend_flutter/lib/main.dart
+++ frontend_flutter/lib/main.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'src/app/uniassist_app.dart';
-import 'src/core/storage/hive_service.dart';
-import 'src/core/services/notification_service.dart';
-import 'src/core/services/sync_service.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  await HiveService.init();
-  await NotificationService.init();
-  SyncService.init();
-
+void main() {
   runApp(const ProviderScope(child: UniAssistApp()));
 }
